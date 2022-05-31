76d3cb9f90048d80e35773d0f3f49c207de8cc78
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-05-31 08:15:42 +0100

    Update Forgotpassword.jsx

--- src/pages/Forgotpassword.jsx
+++ src/pages/Forgotpassword.jsx
@@ -8,8 +8,8 @@ export default function Forgotpassword() {
       <h3 className='text-black text-2xl font-bold text-center'>HerTech</h3>
       <p className='text-black text-sm font-medium text-center mt-1.5'>Forgot password</p>
 
-      <input type="email" placeholder='email address' className='w-full h-9 rounded-lg border-2 border-black px-6 mt-4' />
-      <button className='w-full h-9 rounded-lg bg-black text-white font-bold text-md mt-5'>Reset pasword</button>
+      <input type="email" placeholder='email address' className='w-full h-10 rounded-lg border-2 border-black px-6 mt-4' />
+      <button className='w-full h-10 rounded-lg bg-black text-white font-bold text-md mt-5'>Reset pasword</button>
       <p className='text-center mt-5 text-gray-700 font-medium'>Already have an account? <Link to='/login'><span className='font-bold text-black'>Login</span></Link></p>
       </div>
       
